aa94289b9e18bd9830c97fe7b9b6fd8dc96950a4
ankit <ankitkumar37565@gmail.com>
2023-04-16 10:26:01 +0530

    validation fix

--- controllers/camera.js
+++ controllers/camera.js
@@ -54,19 +54,23 @@ const CameraController = {
                 }
 
                 let camera = await Camera.findOne({ _id: req.params.id });
-                let cameraNetwork2 = await CameraNetwork.findOne({ _id: camera.cameraNetworkId });
-                cameraNetwork2.cameras = cameraNetwork2.cameras.filter((id) => {
-                    return id !== camera._id.toString();
-                });
-                await cameraNetwork2.save();
+                if (camera.cameraNetworkId) {
+                    let cameraNetwork2 = await CameraNetwork.findOne({ _id: camera.cameraNetworkId });
+                    if (cameraNetwork2) {
+                        cameraNetwork2.cameras = cameraNetwork2.cameras.filter((id) => {
+                            return id !== camera._id.toString();
+                        });
+                        await cameraNetwork2.save();
+                    }
+                }
 
                 await Camera.findByIdAndUpdate(req.params.id, update);
                 cameraNetwork1.cameras.push(camera._id.toString());
-                await cameraNetwork1.save()
+                await cameraNetwork1.save();
                 return res.status(200).send({ success: true, message: "Camera Edited Successfully", data: camera });
             } else {
-                let camera = await Camera.findByIdAndUpdate(req.body.id, update);
-                return res.status(200).send({ success: true, message: "Camera Edited Successfully", data: camera });
+                let camera = await Camera.findByIdAndUpdate(req.params.id, update);
+                return res.status(200).send({ success: true, message: "Camera Edited Successfully" });
             }
         } catch (error) {
             console.log("error@editCamera:", error);
@@ -82,11 +86,13 @@ const CameraController = {
             if (camera.cameraNetworkId) {
                 let cameraNetwork = await CameraNetwork.findOne({ _id: camera.cameraNetworkId });
 
-                cameraNetwork.cameras = cameraNetwork.cameras.filter((id) => {
-                    return id !== camera._id.toString();
-                });
+                if (cameraNetwork) {
+                    cameraNetwork.cameras = cameraNetwork.cameras.filter((id) => {
+                        return id !== camera._id.toString();
+                    });
 
-                await cameraNetwork.save();
+                    await cameraNetwork.save();
+                }
             }
             await Camera.findByIdAndDelete(req.params.id);
             return res.status(200).send({ success: true, message: "Camera Deleted Successfully" });
